008c28adcfda0f93eca033dafb641ce5e8cf4edf
Tahnik Mustasin <tahnik@live.co.uk>
2017-12-01 15:45:58 +0000

    Added some comments

--- search_with_google.js
+++ search_with_google.js
@@ -1,3 +1,6 @@
+/**
+ * Called once the extension has been created
+ */
 function onCreated() {
   if (browser.runtime.lastError) {
     console.log(`Error: ${browser.runtime.lastError}`);
@@ -6,16 +9,21 @@ function onCreated() {
   }
 }
 
+/**
+ * Create the context menu
+ */
 browser.contextMenus.create({
   id: "search-with-google",
   title: "Search with Google",
   contexts: ["selection"],
 }, onCreated);
 
+/**
+ * Handle the click event on the menu
+ */
 browser.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "search-with-google":
-      title = info.selectionText.substring(0, 15);
       browser.tabs.create({
         url: `https://www.google.com/search?q=${encodeURIComponent(info.selectionText)}`
       });
